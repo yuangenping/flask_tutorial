9f84999547ae9aeb40dd4abe3f8fadf0424add70
yuangenping <550775082@qq.com>
2026-05-15 16:36:32 +0800

    init

---
+++ src/blog.py
@@ -0,0 +1,156 @@
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    flash,
+    url_for,
+    redirect,
+    g,
+    abort,
+)
+
+from flask.views import MethodView
+
+from .db import get_db
+
+from .user import login_required
+
+blog_bp = Blueprint("blog", __name__, url_prefix="/blog")
+
+
+def get_blog_by_id(id: int):
+    post = (
+        get_db()
+        .execute(
+            """
+            SELECT p.id ,
+                p.title,
+                p.body,
+                p.created,
+                p.author_id,
+                u.username
+                FROM post p 
+                JOIN user u on p.author_id = u.id
+                WHERE p.id = ?
+                    """,
+            (id,),
+        )
+        .fetchone()
+    )
+    return post
+
+
+class Blog(MethodView):
+
+    def get(self):
+        db = get_db()
+        posts = db.execute("""
+                           SELECT p.id ,
+                           p.title,
+                           p.body,
+                           p.created,
+                           p.author_id,
+                           u.username
+                           FROM post p 
+                           JOIN user u on p.author_id = u.id
+                           ORDER BY p.created DESC
+                           """).fetchall()
+        return render_template("blog/index.html", posts=posts)
+
+
+class BlogCreate(MethodView):
+
+    decorators = [login_required]
+
+    def get(self):
+        return render_template("blog/create.html")
+
+    def post(self):
+        form_data = request.form
+        error = ""
+        title = form_data.get("title")
+        body = form_data.get("body")
+        if not title or not body:
+            error = "标题和内容不能为空"
+        else:
+            db = get_db()
+            cursor = db.cursor()
+            try:
+                cursor.execute(
+                    """
+                        INSERT INTO post(author_id,title,body) VALUES(?,?,?) 
+                    """,
+                    (g.user["id"], title, body),
+                )
+                db.commit()
+                return redirect(url_for("blog.index"))
+            except db.Error as e:
+                error = "报错了"
+
+        flash(error)
+
+        return redirect(url_for("blog.create"))
+
+
+class BlogUpdate(MethodView):
+    def get(self, id: int):
+        post = get_blog_by_id(id)
+        if not post:
+            abort(404, f"未找到文章")
+        print(f"文章查询结果: {str(post)}")
+        return render_template("blog/update.html", post=post)
+
+    def post(self, id: int):
+        form_data = request.form
+        title = form_data.get("title")
+        body = form_data.get("body")
+        error = ""
+        if not title or not body or not id:
+            error = "未找到文章"
+        else:
+            db = get_db()
+            cursor = db.cursor()
+            try:
+                cursor.execute(
+                    """
+                            UPDATE post set title = ?, body = ? where id = ?
+                            """,
+                    (title, body, id),
+                )
+                db.commit()
+            except db.Error:
+                error = "修改失败"
+
+        flash(error)
+        return redirect(url_for("blog.index"))
+
+
+class BlogDelete(MethodView):
+    def post(self):
+        id = request.args.get("id")
+        if not id and not isinstance(id, int):
+            abort(400, "参数错误")
+        post = get_blog_by_id(id)
+        if not post:
+            abort(404, "未找到blog")
+
+        db = get_db()
+        cursor = db.cursor()
+        try:
+            cursor.execute(
+                """
+                           DELETE FROM post where id = ?
+                           """,
+                (id,),
+            )
+            db.commit()
+        except db.Error:
+            abort(500)
+
+        return redirect(url_for('blog.index'))
+
+blog_bp.add_url_rule("/index", view_func=Blog.as_view("index"))
+blog_bp.add_url_rule("/create", view_func=BlogCreate.as_view("create"))
+blog_bp.add_url_rule("/update/<int:id>", view_func=BlogUpdate.as_view("update"))
+blog_bp.add_url_rule("/update", view_func=BlogUpdate.as_view("updatePost"))
+blog_bp.add_url_rule("/delete", view_func=BlogDelete.as_view("delete"))
---
+++ src/user.py
@@ -0,0 +1,139 @@
+import functools
+
+from flask import (
+    current_app,
+    Blueprint,
+    jsonify,
+    request,
+    session,
+    redirect,
+    url_for,
+    flash,
+    render_template,
+    g,
+)
+from flask.views import MethodView
+from .db import get_db
+from werkzeug.security import check_password_hash, generate_password_hash
+
+user_bp = Blueprint("user", __name__, url_prefix="/user")
+
+
+@user_bp.before_app_request
+def log_logged_in_user():
+    user_id = session.get("user_id")
+    if not user_id:
+        g.user = None
+    else:
+        user = (
+            get_db()
+            .execute(
+                """
+                    SELECT * FROM user where id = ?
+                """,
+                (user_id,),
+            )
+            .fetchone()
+        )
+        print(f"user--->{user}")
+        g.user = user
+
+
+def login_required(func):
+    @functools.wraps(func)
+    def warpper(*args, **kw):
+        if not g.user:
+            return redirect(url_for("user.login"))
+        return func(*args, **kw)
+
+    return warpper
+
+
+class UserAPI(MethodView):
+
+    def get(self):
+        user_id = session.get("user_id", "未知用户")
+        return f"获取的用户信息 id = {user_id}"
+
+    def post(self):
+        user_desc = request.json["userDesc"]
+        if user_desc is None:
+            return False
+        return True
+
+
+class UserLoginApi(MethodView):
+    def get(self):
+        return render_template("user/login.html")
+
+    def post(self):
+        form_data = request.form
+        user_name = form_data.get("username")
+        password = form_data.get("password")
+        if not user_name or not password:
+            error = "userName or password is Required"
+            return jsonify({"code": 400, "error": error}), 400
+        db = get_db()
+        user = db.execute(
+            """
+            SELECT * FROM user Where username = ?
+            """,
+            (user_name,),
+        ).fetchone()
+        if user is None:
+            error = "not find user"
+        elif not check_password_hash(user["password"], password):
+            error = "password is error"
+        else:
+            session.clear()
+            session["user_id"] = user["id"]
+            return redirect(url_for("blog.index"))
+
+        flash(error)
+        return render_template("user/login.html")
+
+
+class UserRegisterAPI(MethodView):
+
+    def get(self):
+        return render_template("user/register.html")
+
+    def post(self):
+        form_data = request.form
+        error = None
+        if not form_data or not form_data["userName"] or not form_data["password"]:
+            error = "userName or password is Required"
+            return jsonify({"code": 400, "error": error}), 400
+        user_name = form_data["userName"]
+        password = form_data["password"]
+        db = get_db()
+        try:
+            db.execute(
+                """
+                INSERT INTO user(username,password) VALUES(?,?)            
+            """,
+                (user_name, generate_password_hash(password)),
+            )
+            db.commit()
+        except db.IntegrityError:
+            error = f"User {user_name} is already registered."
+        else:
+            return redirect(url_for("user.login"))
+
+        flash(error)
+
+        return redirect(url_for("user.register"))
+
+
+class UserTestRediectLogin(MethodView):
+
+    decorators = [login_required]
+
+    def get(self):
+        pass
+
+
+user_bp.add_url_rule("/", view_func=UserAPI.as_view("detail"))
+user_bp.add_url_rule("/login", view_func=UserLoginApi.as_view("login"))
+user_bp.add_url_rule("/register", view_func=UserRegisterAPI.as_view("register"))
+user_bp.add_url_rule("/test", view_func=UserTestRediectLogin.as_view("test"))
